575d7267d0234652feedc5012e0ca751349d24aa
Alanigma <alanlua2020@hotmail.com>
2019-09-18 11:18:12 -0300

    Tentando colocar número de vezes que aparece

--- script.py
+++ script.py
@@ -3,6 +3,8 @@ id = 0
 idstring = "at0000"
 arquivo = open('fakeADL.txt','w')
 arquivo.close()
+arquivoJS = open('meuscript.js','w')
+arquivo.close()
 arquivoOrdem = open('ordem.txt','w')
 arquivoOrdem.close()
 maior = int(input("Qual é o maior at? Exemplo: se for at0050, digite 50:"))
@@ -45,6 +47,7 @@ while True:
                         encontrado = True
                         contadorletra = 0
                         if vez==1:
+                           palavra = ''
                            for letra in linha:
                                if letra == ' ':
                                    contadorletra+=1
@@ -56,11 +59,29 @@ while True:
                            parar = False
                            arquivo.write(str(contadorletra)+'\n')
                            for letra in linha[0]:
-                               if letra!='[' and parar == False:
-                                   arquivo.write(letra)
                                if letra == '[':
-                                    parar = True
-                           arquivo.write("\n")
+                                   parar = True
+                               if parar == False:
+                                   palavra += letra
+                           pegar1 = 0
+                           for letra in linha[3]:
+                               if pegar1==1:
+                                   if letra.isnumeric():
+                                       palavra += letra
+                                   if letra == '*':
+                                       palavra += letra
+                                   if letra == '}':
+                                       pegar1 = 2
+                               if pegar1 == -1:
+                                   if letra.isnumeric():
+                                       palavra+=letra
+                                   else:
+                                       pegar1 = 1
+                                       palavra += ' '
+                               if letra == '{' and pegar1 != 2:
+                                   pegar1 = -1
+                                   palavra += ' '
+                           arquivo.write(palavra+"\n")
                            vez=2
                         elif vez==2:
                             vez=3
@@ -88,6 +109,7 @@ texto=[]
 
 arquivo = open('fakeADL.txt', 'r')
 for linha in arquivo:
+    linha = linha.replace('\n','')
     while len(texto) in bugados:
         espacos.append(0)
         tipo.append('')
@@ -155,17 +177,18 @@ nome=0
 primeiraSection = 0
 
 arquivoHTML = open('form.html','a')
-arquivoHTML.write('<!DOCTYPE html>\n<html>\n<head>\n<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/form.css"/>\n</head>\n<body>\n<div class="container">\n<form class="ui form ui grid">\n')
+arquivoHTML.write('<!DOCTYPE html>\n<html>\n<head>\n<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/form.css"/>\n'
+                  '<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>\n'
+                  '</head>\n<body>\n<div class="container">\n<form class="ui form ui grid">\n')
 
 for c in range(0, len(tipo2)):
-
-    if tipo2[c] == "COMPOSITION\n":
+    if tipo2[c].split()[0] == "COMPOSITION":
         while len(sectionOn) > 1:
             arquivoHTML.write('</fieldset>\n')
             del sectionOn[len(sectionOn) - 1]
         arquivoHTML.write(f'<h{tamanho}>\n{texto2[c]}</h{tamanho}>\n')
 
-    if tipo2[c] == "SECTION\n":
+    if tipo2[c].split()[0] == "SECTION":
         if sectionOn[len(sectionOn) - 1] == 0:
             tamanhoDaqui = len(sectionOn) + tamanho
             primeiraSection = espacos2[c]
@@ -201,83 +224,105 @@ for c in range(0, len(tipo2)):
                 tamanhoDaqui = len(sectionOn) + tamanho - 1
                 arquivoHTML.write(f'</fieldset>\n<fieldset>\n<legend>\n<h{tamanhoDaqui}>\n{texto2[c]}</h{tamanhoDaqui}>\n</legend>\n')
 
-    if tipo2[c] == "ELEMENT\n":
-        arquivoHTML.write(f'<div class="field fifteen wide column">\n<label for="id{id}"><h{tamanhoDaqui + 1}>\n{texto2[c]}</h{tamanhoDaqui + 1}></label>\n')
-        for q in range(0, len(tipo2)):
-            if tipo2[q] == "DV_TEXT\n":
-                arquivoHTML.write(f'<input class="field eleven wide column" type="text" name="nome{nome}" id="id{id}">\n</div>\n')
-                id+=1
-                espacos2[q] = 0
-                tipo2[q] = "vazio"
-                texto2[q] = "vazio"
-                break
-            if tipo2[q] == "DV_DATE_TIME\n":
-                arquivoHTML.write(f'<input type="datetime-local" name="nome{nome}" id="id{id}"/>\n</div>\n')
-                id+=1
-                espacos2[q] = 0
-                tipo2[q] = "vazio"
-                texto2[q] = "vazio"
-                break
-            if tipo2[q] == "DV_DATE\n":
-                arquivoHTML.write(f'<input type="date" name="nome{nome}" id="id{id}"/>\n</div>\n')
-                id+=1
-                espacos2[q] = 0
-                tipo2[q] = "vazio"
-                texto2[q] = "vazio"
-                break
-            if tipo2[q] == "DV_BOOLEAN\n":
-                arquivoHTML.write(f'<input type="checkbox" value="1" name="nome{nome}" id="id{id}"/>\n</div>\n')
-                id+=1
-                espacos2[q] = 0
-                tipo2[q] = "vazio"
-                texto2[q] = "vazio"
-                break
-            if tipo2[q] == "DV_MULTIMEDIA\n":
-                arquivoHTML.write(f'<input type="file" name="nome{nome}" id="id{id}"/>\n</div>\n')
-                id+=1
-                espacos2[q] = 0
-                tipo2[q] = "vazio"
-                texto2[q] = "vazio"
-                break
-            if tipo2[q] == "DV_TIME\n":
-                arquivoHTML.write(f'<input type="time" name="nome{nome}" id="id{id}"/>\n</div>\n')
+    if tipo2[c].split()[0] == "ELEMENT":
+        arquivoElemento = open('Elemento.html', 'w')
+        arquivoElemento.close()
+        arquivoJS = open('meuscript.js', 'a')
+        arquivoElemento = open('Elemento.html', 'a')
 
-                espacos2[q] = 0
-                tipo2[q] = "vazio"
-                texto2[q] = "vazio"
-                break
-            if tipo2[q] == "DV_DURATION\n" or tipo2[q] == "DV_QUANTITY\n":
-                arquivoHTML.write(f'<input class="field eleven wide column" type="number" name="nome{nome}" id="id{id}"/>\n</div>\n')
+        # if tipo2[c].split()[2] > tipo2[c].split()[1]:
+        #     arquivoJS.write(f'function Function{id}() '+'{'+f'$("#id{id}").append(f\'<div class="field fifteen wide column">\n<label for="id{id}"><h{tamanhoDaqui + 1}>\n{texto2[c]}</h{tamanhoDaqui + 1}></label>\n\');'+'}')
+        arquivoElemento.write(f'<div class="field fifteen wide column">\n<label for="id{id}"><h{tamanhoDaqui + 1}>\n{texto2[c]}</h{tamanhoDaqui + 1}></label>\n')
+        for q in range(0, len(tipo2)):
+            if tipo2[q].split()[0] == "DV_TEXT":
+                arquivoElemento.write(f'<input class="field eleven wide column" type="text" name="nome{nome}" id="id{id}">\n</div>\n')
                 id+=1
                 espacos2[q] = 0
                 tipo2[q] = "vazio"
                 texto2[q] = "vazio"
                 break
-            if tipo2[q] == "DV_PROPORTION\n":
+            if tipo2[q].split()[0] == "DV_DATE_TIME":
+                arquivoElemento.write(f'<input class="field eleven wide column" type="datetime-local" name="nome{nome}" id="id{id}"/>\n</div>\n')
+                id+=1
+                espacos2[q] = 0
+                tipo2[q] = "vazio"
+                texto2[q] = "vazio"
+                break
+            if tipo2[q].split()[0] == "DV_DATE":
+                arquivoElemento.write(f'<input class="field eleven wide column" type="date" name="nome{nome}" id="id{id}"/>\n</div>\n')
+                id+=1
+                espacos2[q] = 0
+                tipo2[q] = "vazio"
+                texto2[q] = "vazio"
+                break
+            if tipo2[q].split()[0] == "DV_BOOLEAN":
+                arquivoElemento.write(f'<input type="checkbox" value="1" name="nome{nome}" id="id{id}"/>\n</div>\n')
+                id+=1
+                espacos2[q] = 0
+                tipo2[q] = "vazio"
+                texto2[q] = "vazio"
+                break
+            if tipo2[q].split()[0] == "DV_MULTIMEDIA":
+                arquivoElemento.write(f'<input class="field eleven wide column" type="file" name="nome{nome}" id="id{id}"/>\n</div>\n')
+                id+=1
+                espacos2[q] = 0
+                tipo2[q] = "vazio"
+                texto2[q] = "vazio"
+                break
+            if tipo2[q].split()[0] == "DV_TIME":
+                arquivoElemento.write(f'<input class="field eleven wide column" type="time" name="nome{nome}" id="id{id}"/>\n</div>\n')
+                id+=1
+                espacos2[q] = 0
+                tipo2[q] = "vazio"
+                texto2[q] = "vazio"
+                break
+            if tipo2[q].split()[0] == "DV_DURATION" or tipo2[q] == "DV_QUANTITY":
+                arquivoElemento.write(f'<input class="field eleven wide column" class="field eleven wide column" type="number" name="nome{nome}" id="id{id}"/>\n</div>\n')
+                id+=1
+                espacos2[q] = 0
+                tipo2[q] = "vazio"
+                texto2[q] = "vazio"
+                break
+            if tipo2[q].split()[0] == "DV_PROPORTION":
                 espacos2[q] = 0
                 tipo2[q] = "vazio"
                 texto2[q] = "vazio"
                 for q2 in range(0, 11):
-                    arquivoHTML.write('<div class="ui radio checkbox">\n')
-                    arquivoHTML.write(f'<input type="radio" value="{q2}" name="nome{nome}" id="id{id}"/>\n<label for="id{id}">\n{q2}</label>\n</div>\n')
+                    arquivoElemento.write('<div class="ui radio checkbox">\n')
+                    arquivoElemento.write(f'<input class="field eleven wide column" type="radio" value="{q2}" name="nome{nome}" id="id{id}"/>\n<label for="id{id}">\n{q2}</label>\n</div>\n')
                     id += 1
                 break
-            if tipo2[q] == "DV_CODED_TEXT\n":
-                arquivoHTML.write('<select>\n')
-                arquivoHTML.write(f'<option value=""></option>\n')
+            if tipo2[q].split()[0] == "DV_CODED_TEXT":
+                arquivoElemento.write(f'<select id="id{id}">\n')
+                id+=1
+                arquivoElemento.write(f'<option value=""></option>\n')
                 espacos2[q] = 0
                 tipo2[q] = "vazio"
                 texto2[q] = "vazio"
                 for q2 in range(q + 1, len(tipo2)):
-                    if tipo2[q2] == "CODE_PHRASE\n":
-                         arquivoHTML.write(f'<option value="{texto2[q2]}">{texto2[q2]}</option>\n')
+                    if tipo2[q2].split()[0] == "CODE_PHRASE":
+                         arquivoElemento.write(f'<option value="{texto2[q2]}">{texto2[q2]}</option>\n')
                          espacos2[q2] = 0
                          tipo2[q2] = "vazio"
                          texto2[q2] = "vazio"
                     else: break
-                arquivoHTML.write('</select>\n')
-                arquivoHTML.write('</div>\n')
+                arquivoElemento.write('</select>\n')
+                arquivoElemento.write('</div>\n')
                 break
+        arquivoElemento.close()
+        arquivoElemento = open('Elemento.html', 'r')
+        contadorID=0
+        for vezes in range(int(tipo2[c].split()[1])):
+            for linha in arquivoElemento:
+                for linhaID in range(id, -1):
+                    if f'id{linhaID}' in linha:
+                        linha = linha.replace(f'id{linhaID}', f'id{linhaID+contadorID}')
+                        contadorID += 1
+                        break
+                arquivoHTML.write(linha)
+        id += contadorID
+        arquivoElemento.close()
+        arquivoJS.close()
         nome += 1
 
 while len(sectionOn) > 1:
